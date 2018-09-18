74b88cf531b68e7958dece3cd9f304cff0657ab0
unrealisher <unrealmpa3@mail.ru>
2018-09-18 17:45:33 +0300

    Выполнил 2-е задания и убрал outline

--- js/map.js
+++ js/map.js
@@ -17,6 +17,8 @@ var MAXY = 500;
 var MARGINX = 20;
 var MARGINY = 40;
 
+var ESCKEY = 27;
+
 var offers = [
   {
     'author': {
@@ -197,18 +199,59 @@ var offers = [
 ];
 
 var map = document.querySelector('.map');
-var mapCardTemplate = document.querySelector('template').content.querySelector('.map__card');
-var mapPins = map.querySelector('.map__pins');
-var mapFiltersContainer = map.querySelector('.map__filters-container');
 
-map.classList.remove('map--faded');
+var mainPin = map.querySelector('.map__pin--main');
+var mapPins = map.querySelector('.map__pins');
+
+var disableForm = function() {
+  var fieldsets = document.querySelector('.notice__form').querySelectorAll('fieldset');
+  for (var i = 0; i < fieldsets.length; i++) {
+    fieldsets[i].disabled = true;
+  }
+}
+
+var initOffersPins = function (offers) {
+  var fragment = document.createDocumentFragment();
+  var mapPins = map.querySelector('.map__pins');
+  for (var i = 0; i < offers.length; i++) {
+    var newElement = document.createElement('button');
+    newElement.style = 'left: ' + (offers[i].location.x + MARGINX) + 'px; top: ' + (offers[i].location.y + MARGINY) + 'px;';
+    newElement.className = 'map__pin hidden';
+    newElement.innerHTML = '<img src="' + offers[i].author.avatar + '" width="40" height="40" draggable="false">';
+    fragment.appendChild(newElement);
+  }
+  mapPins.appendChild(fragment);
+}
+
+var showPins = function () {
+  var pins = map.querySelectorAll('.map__pin');
+  for (var i = 0; i < pins.length; i++) {
+    pins[i].classList.remove('hidden');
+  }
+}
+
+var activeForm = function() {
+  var fieldsets = document.querySelector('.notice__form').querySelectorAll('fieldset');
+  for (var i = 0; i < fieldsets.length; i++) {
+    fieldsets[i].disabled = false;
+  }
+}
+
+var onMainPinFirstMouseup = function () {
+  map.classList.remove('map--faded');
+  showPins();
+  var form = document.querySelector('.notice__form');
+  form.classList.remove('notice__form--disabled');
+  activeForm();
+  mainPin.removeEventListener('mouseup', onMainPinFirstMouseup);
+}
 
 var typeOfHouse = function (houseType) {
   var returnType;
-  if (houseType == 'flat') {
+  if (houseType === 'flat') {
     returnType = 'Квартира';
   }
-  else if (houseType == 'house') {
+  else if (houseType === 'house') {
     returnType = 'Дом';
   }
   else {
@@ -217,48 +260,83 @@ var typeOfHouse = function (houseType) {
   return returnType;
 }
 
-var initOffersPins = function (offers) {
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < offers.length; i++) {
-    var newElement = document.createElement('button');
-    newElement.style = 'left: ' + (offers[i].location.x + MARGINX) + 'px; top: ' + (offers[i].location.y + MARGINY) + 'px;';
-    newElement.className = 'map__pin';
-    newElement.innerHTML = '<img src="' + offers[i].author.avatar + '" width="40" height="40" draggable="false">';
-    fragment.appendChild(newElement);
+
+var initMapCard = function (offer) {
+  var mapCardElement = document.querySelector('template').content.querySelector('.map__card').cloneNode(true);
+  var popupAvatar = mapCardElement.querySelector('.popup__avatar');
+  var popupClose = mapCardElement.querySelector('.popup__close');
+  var popupTitle = mapCardElement.querySelector('h3');
+  var popupAdress = mapCardElement.querySelectorAll('p')[0];
+  var popupPrice = mapCardElement.querySelector('.popup__price');
+  var popupType = mapCardElement.querySelector('h4');
+  var popupRoom = mapCardElement.querySelectorAll('p')[1];
+  var popupCheck = mapCardElement.querySelectorAll('p')[2];
+  var popupFeature = mapCardElement.querySelector('.popup__features');
+  var popupDescription = mapCardElement.querySelectorAll('p')[3];
+  var popupPhoto = mapCardElement.querySelector('.popup__pictures');
+  popupAvatar.src = offer.author.avatar;
+  popupTitle.textContent = offer.offer.title;
+  popupPrice.innerHTML = offer.offer.price + '&#x20bd;/ночь';
+  popupType.textContent = typeOfHouse(offer.offer.type);
+  popupRoom.textContent = offer.offer.rooms + ' комнаты для ' + offer.offer.guests + ' гостей';
+  popupCheck.textContent = 'Заезд после ' + offer.offer.checkin + ', выезд до ' + offer.offer.checkout;
+  popupFeature.textContent = null;
+  for (var j = 0; j < offer.offer.features.length; j++) {
+    var newFeature = document.createElement('li');
+    newFeature.className = 'feature feature--' + offer.offer.features[j];
+    popupFeature.appendChild(newFeature);
   }
-  mapPins.appendChild(fragment);
+  popupDescription.textContent = offer.offer.description;
+  var previousMapCard = map.querySelector('.map__card');
+  if (previousMapCard != null) {
+    map.removeChild(previousMapCard);
+  }
+  popupClose.addEventListener('click', mapCardRemove);
+  var mapFiltersContainer = map.querySelector('.map__filters-container');
+  map.insertBefore(mapCardElement, mapFiltersContainer);
+
 }
 
-var initMapCards = function (offers, mapCardTemplate) {
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < 1; i++) {
-    var mapCardElement = mapCardTemplate.cloneNode(true);
-    var popupAvatar = mapCardElement.querySelector('.popup__avatar');
-    var popupTitle = mapCardElement.querySelector('h3');
-    var popupAdress = mapCardElement.querySelectorAll('p')[0];
-    var popupPrice = mapCardElement.querySelector('.popup__price');
-    var popupType = mapCardElement.querySelector('h4');
-    var popupRoom = mapCardElement.querySelectorAll('p')[1];
-    var popupCheck = mapCardElement.querySelectorAll('p')[2];
-    var popupFeature = mapCardElement.querySelector('.popup__features');
-    var popupDescription = mapCardElement.querySelectorAll('p')[3];
-    var popupPhoto = mapCardElement.querySelector('.popup__pictures');
-    popupAvatar.src = offers[i].author.avatar;
-    popupTitle.textContent = offers[i].offer.title;
-    popupPrice.innerHTML = offers[i].offer.price + '&#x20bd;/ночь';
-    popupType.textContent = typeOfHouse(offers[i].offer.type);
-    popupRoom.textContent = offers[i].offer.rooms + ' комнаты для ' + offers[i].offer.guests + ' гостей';
-    popupCheck.textContent = 'Заезд после ' + offers[i].offer.checkin + ', выезд до ' + offers[i].offer.checkout;
-    popupFeature.textContent = null;
-    for (var j = 0; j < offers[i].offer.features.length; j++) {
-      var newFeature = document.createElement('li');
-      newFeature.className = 'feature feature--' + offers[i].offer.features[j];
-      popupFeature.appendChild(newFeature);
-    }
-    popupDescription.textContent = offers[i].offer.description;
-    fragment.appendChild(mapCardElement);
-  }
-  map.insertBefore(fragment, mapFiltersContainer);
+var mapCardRemove = function() {
+  var activeMapCard = map.querySelector('.map__card');
+  var activePin = map.querySelector('.map__pin--active');
+  activePin.classList.remove('map__pin--active');
+  map.removeChild(activeMapCard);
 }
+
+var onMapCardEscPress = function (evt) {
+  if (evt.keyCode === ESCKEY) {
+    mapCardRemove();
+    document.removeEventListener('keydown', onMapCardEscPress);
+  }
+}
+
+mapPins.addEventListener('click', function(evt) {
+  var objTarget;
+  var previousPin = map.querySelector('.map__pin--active');
+  if (evt.target.localName === 'img') {
+    objTarget = evt.target.parentElement;
+  }
+  else if (evt.target.localName === 'button') {
+    objTarget = evt.target;
+  }
+  if (objTarget != null && !objTarget.classList.contains('map__pin--main')) {
+    if (previousPin != null) {
+      previousPin.classList.remove('map__pin--active');
+    }
+    objTarget.classList.add('map__pin--active');
+    var objImageSrc = objTarget.querySelector('img').src;
+    for (var i = 0; i < offers.length; i++) {
+      if (objImageSrc.indexOf(offers[i].author.avatar) != -1) {
+        initMapCard(offers[i]);
+        break;
+      }
+    }
+    document.addEventListener('keydown', onMapCardEscPress);
+  }
+});
+
+disableForm();
 initOffersPins(offers);
-initMapCards(offers, mapCardTemplate);
+
+mainPin.addEventListener('mouseup', onMainPinFirstMouseup);
